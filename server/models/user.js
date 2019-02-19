92ca79db5fc0f1e6100f0cd3216d29e08ff9601c
Sandip Guchait <35764971+sandipguchait@users.noreply.github.com>
2019-02-20 00:14:18 +0530

    hashing password

--- server/models/user.js
+++ server/models/user.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const SALT = 10;
 
 const userSchema = mongoose.Schema({
     email:{
@@ -12,7 +14,28 @@ const userSchema = mongoose.Schema({
         required: true,
         minlength:6
     }
+});
+
+
+//Hashing the password before saving to database
+userSchema.pre('save', function(next){
+    var user = this;
+
+    if(user.isModified('password')) {
+        bcrypt.genSalt(SALT, function(err,salt){
+            if(err) return next(err);
+            
+            bcrypt.hash(user.password, salt , function (err,hash){
+                if(err) return next(err);
+                user.password = hash;
+                next();
+            })
+        })
+    } else(
+        next()
+    )
 })
+
 
 const User = mongoose.model('User', userSchema);
 
